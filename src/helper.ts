4c9eca8870fd10c6f344966db61a45222d9b416d
Arturo Sanchez <asanchez@orthocana.com>
2022-03-22 17:56:52 -0600

    # ENGINE/SHADERS * Fixed navigator.gpu validation. * Fixed fragment shader.
    Signed-off-by: Arturo Sanchez <asanchez@orthocana.com>

--- src/helper.ts
+++ src/helper.ts
@@ -1,9 +1,11 @@
 export const CheckWebGpu = () => {
     let result = 'Great, your browser supports WebGPU!';
-    if(navigator.gpu){
+
+    if(!navigator.gpu) {
         result = `Your current browser does not support WebGPU! Make sure you are on a system with WebGPU enabled. 
         Currently, WebGPU is supported in <a href="https/google.com/chrome/canary">Chrome Canary</a> with the flag "enable-unsafe-webgpu" enabled`;
     }
+
     const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;
     if(canvas){
         const div = document.getElementsByClassName('item2')[0] as HTMLDivElement;
